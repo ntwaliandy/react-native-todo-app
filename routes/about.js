d8fd0ce663bc632a29038d1d4d1a62e357d7b0cb
ntwaliandy <ntwaliandy90@gmail.com>
2021-12-07 09:09:22 +0300

    first commit

---
+++ routes/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { createDrawerNavigator, createAppContainer } from "react-navigation";
+import { Text, View } from 'react-native';
+
+export default class AboutScreen extends Component {
+  render() {
+    return (
+      <View>
+      <Text> This is About screen </Text>
+      </View>
+    )
+  }
+}
